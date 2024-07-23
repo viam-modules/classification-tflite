c958eff5edbeca209c34545736bef04834ba69c2
Etai Shuchatowitz <etai.shuchatowitz@gmail.com>
2024-07-23 11:19:27 -0400

    new script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <directory>"
+    exit 1
+fi
+
+directory=$1
+
+if [ ! -d "$directory" ]; then
+    echo "Error: Directory '$directory' not found."
+    exit 1
+fi
+
+# Change the working directory to the provided directory
+cd "$directory" || exit
+
 python3 setup.py sdist --formats=gztar
 pip install dist/model-0.1.tar.gz
 
