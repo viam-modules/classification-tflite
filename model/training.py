b6705794ec9ccc4df73b91452a1f36e1d0b80a81
etai-shuchatowitz <4420609+etai-shuchatowitz@users.noreply.github.com>
2024-07-24 11:30:29 -0400

    Data 2821/workflows (#2)
    * Add setup file
    
    * Add logic
    
    * Change parsing
    
    * Add writer stuff
    
    * Add conversion stuff
    
    * DATA-2787 - Trying to link to Jira
    
    * v1
    
    * action
    
    * move description
    
    * trying again
    
    * trying again
    
    * trying again
    
    * trying again
    
    * trying again
    
    * trying again
    
    * correct name
    
    * trying again
    
    * build in docker
    
    * trying again
    
    * removing poetry
    
    * trying to add main workflow
    
    * trying again
    
    * trying again
    
    * using current branch
    
    * wrong path
    
    * formatting
    
    * formatting part deux
    
    * trying again
    
    * trying again
    
    * trying again
    
    * trying again
    
    * trying again
    
    * trying again
    
    * trying again
    
    * trying again
    
    * trying again
    
    * trying again
    
    * shell
    
    * test
    
    * test
    
    * gitignore
    
    * trying again
    
    * trying again
    
    * no shell
    
    * trying again
    
    * end quote
    
    * pull and tag
    
    * build stage
    
    * trying again
    
    * trying again
    
    * trying again
    
    * trying again
    
    * trying again
    
    * trying again
    
    * trying again
    
    * new workflow
    
    * install viam cli
    
    * better cli
    
    * org id
    
    * org id part 2
    
    * trying to test
    
    * trying to test
    
    * pip install pytest
    
    * running unit test
    
    * pr changes
    
    * stop and force remove
    
    * no stop
    
    * reusable
    
    * trying again
    
    * dummy commit for workflow
    
    * yaml, not yml
    
    * dummy commit for workflow
    
    * common workflows
    
    * pass secrets
    
    * trying again
    
    * dummy commit for workflow
    
    * test fail
    
    * fixing it up
    
    * new script
    
    * dummy commit for workflow
    
    * correct name
    
    * correct test
    
    * passing test
    
    * cleanup
    
    * dummy commit for workflow
    
    * test_script_name
    
    * pr fixes
    
    * testing new deploy
    
    * back to good
    
    * final test
    
    * single label
    
    * reset for merge
    
    ---------
    
    Co-authored-by: Tahiya Salam <tahiyasalam@gmail.com>

---
+++ model/training.py
@@ -0,0 +1,19 @@
+import argparse
+import json
+import os
+import typing as ty
+import tensorflow as tf
+from keras import Model
+import numpy as np
+
+from tflite_support.metadata_writers import image_classifier
+from tflite_support.metadata_writers import writer_utils
+
+if __name__ == "__main__":
+    file_path = "new_file.txt"
+
+    # Open the file in write mode
+    with open(file_path, "w") as file:
+        # Write content to the file
+        file.write("Hello, this is a new text file created using open() function.")
+        print(f"I wrote to {file_path}")
