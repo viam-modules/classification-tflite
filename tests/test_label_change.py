b09f60e31e19a58a0b412e8a718e67d3d8029ee5
Vignesh <vpandiarajan20@gmail.com>
2024-09-24 14:32:03 -0400

    lint

--- tests/test_label_change.py
+++ tests/test_label_change.py
@@ -5,8 +5,8 @@ def test_file_exists():
     assert os.path.exists("labels.txt")
     assert os.path.exists("classification_model.tflite")
 
+
 def test_file_contains_green_square():
-    with open('labels.txt', 'r') as file: 
+    with open("labels.txt", "r") as file:
         content = file.read()
     assert "green_square" in content
-
